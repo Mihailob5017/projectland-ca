3993387274680943b13fbcc60eb0b4ac44d75e25
Mihailob5017 <mihailob5017@gmail.com>
2021-12-20 15:16:51 +0100

    finished the main mechanic of the survey

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 // Components
 import Input from './components/input/input.component';
+import Question from './components/question/question.component';
 
 // Helper Obj
 const surveyObject = { title: '', deadline: '', questions: [] };
@@ -12,7 +13,21 @@ const App = () => {
 	const [survey, setSurvey] = useState(surveyObject);
 
 	const setSurveyHandler = (name, value) => {
-		const newValue = Object.assign({}, survey, { [name]: value });
+		let newValue;
+		if (name === 'duplicate-question') {
+			newValue = Object.assign({}, survey, {
+				questions: [
+					...survey.questions,
+					survey.questions[survey.questions.length - 1],
+				],
+			});
+		} else if (name === 'question') {
+			newValue = Object.assign({}, survey, {
+				questions: [...survey.questions, value],
+			});
+		} else {
+			newValue = Object.assign({}, survey, { [name]: value });
+		}
 		setSurvey(newValue);
 	};
 
@@ -32,6 +47,30 @@ const App = () => {
 					value={survey['deadline']}
 					handleChange={setSurveyHandler}
 					label='Survey Deadline'
+				/>
+			</div>
+			<div className='app-body'>
+				<label className='questions-label'>Survey Questions:</label>
+				{survey['questions'].map((question, i) => {
+					return (
+						<div key={i} className='single-question-info'>
+							<label className='single-q-label'>
+								<label className='text-bold'>{i + 1 + '. '}</label>
+								{question.title}
+							</label>
+							<label className='single-a-label'>
+								<label className='text-regular'>Answers:</label>
+								{question.answers.map((answer) => `  "${answer}"  `)}
+							</label>
+						</div>
+					);
+				})}
+			</div>
+			<div className='app-questions'>
+				<Question
+					addQuestion={(value) => setSurveyHandler('question', value)}
+					doesPrevQuesionExist={survey.questions.length > 0}
+					duplicateQuestion={() => setSurveyHandler('duplicate-question')}
 				/>
 			</div>
 		</div>
